ca8ad4322f6b4735e446dc11db228ce5e25010c7
Pavel Demin <pavel.demin@cern.ch>
2007-10-22 16:55:30 +0000

    first commit of working code

---
+++ kitgen/tars/download.sh
@@ -0,0 +1,8 @@
+wget http://www.equi4.com/pub/tk/tars/kitgen.tgz
+wget http://www.equi4.com/pub/tk/tars/vfs.tar.gz
+wget http://www.equi4.com/pub/tk/tars/zlib.tar.gz
+wget http://www.equi4.com/pub/tk/tars/vqtcl.tgz
+wget http://www.equi4.com/pub/tk/tars/tcl.tar.gz
+wget http://pascal.scheffers.net/software/tclDict-8.5.2.tar.gz
+wget http://www.starfishsystems.ca/cgi-bin/download?name=starfish
+wget http://www-unix.globus.org/ftppub/gt4/4.0/4.0.5/installers/src/gt4.0.5-all-source-installer.tar.gz
