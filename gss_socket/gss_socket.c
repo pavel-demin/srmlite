84730a295c34001f48ae277cb91b019ba5728d15
Pavel Demin <pavel.demin@cern.ch>
2008-04-25 22:49:58 +0000

    delete broken context in case of error

--- gss_socket/gss_socket.c
+++ gss_socket/gss_socket.c
@@ -919,6 +919,14 @@ GssHandshake(GssState *statePtr)
       statePtr->flags |= GSS_TCL_EOF;
       statePtr->errorCode = 0;
 
+      if(statePtr->gssContext != GSS_C_NO_CONTEXT)
+      {
+        majorStatus = gss_delete_sec_context(&minorStatus,
+                                             &statePtr->gssContext,
+                                             GSS_C_NO_BUFFER);
+        statePtr->gssContext = GSS_C_NO_CONTEXT;
+      }
+
       globus_gss_assist_display_status(
         stderr, "Failed to establish security context: ",
         majorStatus, minorStatus, 0);
