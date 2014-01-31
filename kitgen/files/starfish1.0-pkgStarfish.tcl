e0de3ca502dca264e473465fd0469dbb69755844
Pavel Demin <pavel.demin@cern.ch>
2014-01-31 07:52:29 +0000

    add new files

---
+++ kitgen/files/starfish1.0-pkgStarfish.tcl
@@ -0,0 +1,17 @@
+#
+# Tcl package load file for Starfish remote execution environment
+#
+# Copyright (c) 2012 Starfish Systems (www.starfishsystems.ca)
+# All rights reserved.
+#
+# This software may be distributed under the terms of the
+# GNU General Public License.  A copy of this License has
+# been included with the software.  Please see the License
+# for warranty and conditions of use.
+#
+
+package provide starfish	$starfish::version
+
+package require starfishGui	$starfish::version
+package require starfishLib	$starfish::version
+package require starfishBase	$starfish::version
